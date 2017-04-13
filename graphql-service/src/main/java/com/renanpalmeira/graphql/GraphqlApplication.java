6c9a1db34047092023ddd5c8d6a3cebe211a76b6
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-04-13 03:03:23 -0300

    Initial graph-service and call api-service with Feign and Hystrix

--- graphql-service/src/main/java/com/renanpalmeira/graphql/GraphqlApplication.java
+++ graphql-service/src/main/java/com/renanpalmeira/graphql/GraphqlApplication.java
@@ -2,7 +2,11 @@ package com.renanpalmeira.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+@EnableCircuitBreaker
+@EnableFeignClients
 @SpringBootApplication
 public class GraphqlApplication {
 
---
+++ graphql-service/src/main/java/com/renanpalmeira/graphql/model/Activity.java
@@ -0,0 +1,10 @@
+package com.renanpalmeira.graphql.model;
+
+import lombok.Data;
+
+@Data
+public class Activity {
+    private String name;
+    private String category;
+    private String tag;
+}
