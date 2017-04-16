90f4bdb5457e094a7f61740ffa81899d7ff25d9b
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-04-16 01:50:58 -0300

    Initial UI service with Vue and AUI components

---
+++ ui-service/src/components/dashboard/FnHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="aui-header">
+        <div class="aui-header-primary">
+            <h1 id="logo" class="aui-header-logo aui-header-logo-aui">
+                <a href="#">
+                    <span class="aui-header-logo-device">finance</span>
+                </a>
+            </h1>
+            <ul class="aui-nav">
+                <li>
+                    <form class="aui-quicksearch" method="post" action="/foo">
+                        <label class="assistive" for="quicksearchid">Search</label>
+                        <input type="text" name="quicksearchname" placeholder="Quick Search" class="search" id="quicksearchid">
+                    </form>
+                </li>
+            </ul>
+        </div>
+        <div class="aui-header-secondary">
+            <ul class="aui-nav">
+                <li>
+                    <a href="http://example.com/">
+                        <span class="aui-icon aui-icon-small aui-iconfont-help">Help</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import Octicon from 'vue-octicon/components/Octicon.vue'
+
+    export default {
+        components: {
+            Octicon
+        }
+    }
+</script>
---
+++ ui-service/src/router/index.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        path: '/',
+        name: 'dashboard.index',
+        component: require('../components/dashboard/FnIndex.vue')
+    }
+]
