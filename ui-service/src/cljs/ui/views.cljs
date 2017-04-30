dbd9d7625245fe9b2279bb92074c616ce8fb47df
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-04-30 01:19:00 -0300

    Initial using AUI in ui-service. https://docs.atlassian.com/aui/

--- ui-service/src/cljs/ui/views.cljs
+++ ui-service/src/cljs/ui/views.cljs
@@ -1,6 +1,5 @@
 (ns ui.views
-    (:require [re-frame.core :as re-frame]
-              [reagent-material-ui.core :refer [RaisedButton]]))
+  (:require [re-frame.core :as re-frame]))
 
 
 ;; counter
@@ -9,17 +8,23 @@
   (let [c (re-frame/subscribe [:all-counters])]
     (fn []
       [:div (str "Testando" @c)
-        [:button {:on-click #(re-frame/dispatch [:increment-counter c])} "Click me"]
-        [RaisedButton {:label (str "Click to inc " @c) :primary true}]])))
+       [:button {:on-click #(re-frame/dispatch [:increment-counter c])} "Click me"]])))
 
 ;; home
 
 (defn home-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div (str "Hello from " @name ". This is the Home Page.")
+      [:div {:class "page"}
+       [:header {:id "header"}
+        [:nav {:class "aui-header aui-dropdown2-trigger-group"}
+         [:div {:class "aui-header-inner"}
+          [:div {:class "aui-header-primary"}
+           [:h1 {:id "logo" :class "aui-header-logo aui-header-logo-aui"}
+            [:a {:href "#/about"}
+             [:span {:class "aui-header-logo-device"} "AUI"]]]]]]]
+       [:span {:class "h1 aui-lozenge aui-lozenge-success"} (str "Hello from " @name ". This is the Home Page.")]
        [:div [:a {:href "#/about"} "go to About Page"]]])))
-
 
 ;; about
 
